b74f34240bd17bdaa22bd13e3d54cb4303bdeecd
Natalie Gillam <ngillam@hotmail.co.uk>
2024-01-15 17:30:26 +0000

    added portfolio project images, portfolioData.js file, Header component and Footer component

---
+++ src/assets/data/portfolioData.js
@@ -0,0 +1,14 @@
+import portfolioImg01 from "../images/portfolio-01.jpg";
+
+const projects = [
+{
+id:"01",
+imgUrl: portfolioImg01,
+category: "Web Design",
+title: "Finance Technology Website",
+description: "Description goes here",
+technologies: ["React", "Tailwind"],
+siteUrl: "",
+gitHub: "",
+},
+]
