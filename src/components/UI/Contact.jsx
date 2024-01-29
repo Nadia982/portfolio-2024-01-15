3cb6c5dddd6518b17afabc574716ded1594431cd
Natalie Gillam <ngillam@hotmail.co.uk>
2024-01-29 23:45:58 +0000

    added form elements to contact component

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -18,7 +18,25 @@ const Contact = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>{" "}
           </div>
-         
+          <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8">
+            <form action="" className="w-full">
+                <div className="mb-5">
+                    <input type="text" placeholder="Enter your name" className="w-full p-3 rounded-[5px] placeholder-gray-900 text-bg-headingColor" />
+                </div>
+                <div className="mb-5">
+                    <input type="text" placeholder="Enter your email" className="w-full p-3 rounded-[5px] placeholder-gray-900"  />
+                </div>
+                <div className="mb-5">
+                    <input type="text" placeholder="Subject" className="w-full p-3 rounded-[5px] placeholder-gray-900" />
+                </div>
+                <div className="mb-5">
+                    <textarea type="text" rows={3} placeholder="Write your message" className="w-full p-3 rounded-[5px] placeholder-gray-900" />
+                </div>
+                <button className="w-full p-3 text-[1.5rem] rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150">
+Send Message
+                </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
