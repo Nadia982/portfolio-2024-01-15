2a2773709df1d81d5d8763d7c7756c02ca86a609
Natalie Gillam <ngillam@hotmail.co.uk>
2024-01-31 23:13:02 +0000

    added footer with linkedin, github and mail icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-[#12141e] pt-12">
+    <div className="container">
+      <div className="sm:flex items-center justify-between md:gap-8">
+        <div className="w-full">
+          <h2 className="text-[2rem] leading-10 text-center text-white font-[600] md:text-[2rem]">
+            Get in touch
+          </h2>
+          <div className="flex h-20 items-center justify-center gap-4 mt-0">
+          <span>
+                    <a aria-label="link to Natalie Gillam's LinkedIn profile" href="#linkedin" className="text-white text-[3rem] font[-700]"><i class="ri-linkedin-box-fill"></i></a>
+                </span>
+                <span className="mb-1">
+                    <a aria-label="link to Natalie Gillam's GitHub profile" href="#linkedin" className="text-white text-[3rem] font[-700]"><i class="ri-github-fill"></i></a>
+                </span>
+                <span>
+                    <a aria-label="email Natalie Gillam" href="#mail" className="text-white text-[3rem] font[-700]"><i class="ri-mail-line"></i></a>
+                </span>
+                
+                </div>
+        </div>
+      </div>
+    </div>
+    </footer>
   )
 }
 
