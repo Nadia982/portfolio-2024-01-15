53bb6fcca723de778e0f804f7c86e79ff465d0a0
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-03 22:59:45 +0000

    tweaks to Hero section

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -39,10 +39,10 @@ const Hero = () => {
                 className=" text-headingColor mt-8 font-[500] text-[1rem] leading-7"
               >I have been building websites with React and JavaScript since 2022. I love creating sites and apps that solve real-world problems, and engage and delight users.</p>
               
-              <h3 className="text-smallTextColor text-[1.3rem] mt-8 md:mb-0 sm:mb-4 mr-0 font-[700]">Let's Connect!</h3>
-              <div className="flex h-20 items-center gap-4 mt-0">
-                <span className="text-smallTextColor text-[1rem] font-[500]">Feel free to send me a <a aria-label="link to Natalie Gillam's LinkedIn profile" href="#linkedin" className="font-[700] underline underline-offset-2">LinkedIn connection request</a></span>
-                <span>
+              <h3 className="text-smallTextColor text-[1.3rem] mt-8 md:mb-0 mr-0 font-[700]">Let's Connect!</h3>
+              <div className="flex items-center gap-4 h-20">
+                <span className="text-smallTextColor text-[1rem] font-[500] basis-1/2">Feel free to send me a <a aria-label="link to Natalie Gillam's LinkedIn profile" href="#linkedin" className="font-[700] underline underline-offset-2">LinkedIn connection request</a></span>
+                <span >
                     <a aria-label="link to Natalie Gillam's LinkedIn profile" href="#linkedin" className="text-headingTextColor text-[3rem] font[-700]"><i class="ri-linkedin-box-fill"></i></a>
                 </span>
                 <span className="mb-1">
