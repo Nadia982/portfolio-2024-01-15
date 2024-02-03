06f7c845fe91e42e97928c79d13eecd190b1c43d
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-03 22:13:30 +0000

    updated Modal so it doesn't overlap with sticky menu

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -4,14 +4,14 @@ import projects from "../../assets/data/portfolioData";
 const Modal = ({activeID, setShowModal}) => {
   const project = projects.find(project => project.id ==activeID);
   return (
-    <div className = "w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40">
-      <div className = "max-w-[600px] absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5">
-        <div>
+    <div className ="w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40">
+      <div className = "mt-14 flex-column m-auto items-end justify-end border-gray-500 border-4 h-[80vh] w-11/12 md:max-w-[600px] md:w-full absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5">
+        <div className ="w-1/2 m-auto border-4 border-gray-300 rounded-[8px]">
           <figure>
             <img className="rounded-[8px]" src={project.imgUrl} alt={project.imgUrl}/>
           </figure>
         </div>
-        <div>
+        <div className="m-auto">
           <h2 className="text-2xl text-headingColor font-[700] my-5">{project.title}
           </h2>
           <p className = "text-[1rem] leading-7 text-smallTextColor">{project.description}</p>
